31e8833eeabde823ca7e79ca58331ee760141ac3
leemeem <wind2cloud@outlook.com>
2018-03-27 16:25:15 +0800

    DEV : Update app views.

---
+++ app/src/main/java/org/neen/demo/settings/SettingsActivity.java
@@ -0,0 +1,21 @@
+package org.neen.demo.settings;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import org.neen.demo.R;
+
+/**
+ * Created by NQSKY on 2018/3/27.
+ */
+
+public class SettingsActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+}
---
+++ app/src/main/java/org/neen/demo/settings/fragment/SettingsFragmentA.java
@@ -0,0 +1,12 @@
+package org.neen.demo.settings.fragment;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by NQSKY on 2018/3/27.
+ */
+
+public class SettingsFragmentA extends PreferenceFragment{
+
+
+}
